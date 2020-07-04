524998b1259c8053f443eeef9eed649dfe1edc0f
Cleanderson Lobo <cleandersonlobo@gmail.com>
2020-07-04 09:40:03 -0300

    Projeto Iniciado

---
+++ src/routes.tsx
@@ -0,0 +1,23 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from 'screens/Home/Home';
+import { transitionSpec } from 'helpers/utils';
+
+const MainStack = createStackNavigator();
+
+const RouteStack: React.FC = () => {
+  return (
+    <MainStack.Navigator initialRouteName="Home">
+      <MainStack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          transitionSpec,
+        }}
+      />
+    </MainStack.Navigator>
+  );
+};
+
+export default RouteStack;
---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+const Home: React.FC = () => {
+  return (
+    <View>
+      <Text />
+    </View>
+  );
+};
+
+export default Home;
